6ee0e1794eaad182b6642f90559aea8878345b19
Dxvah <idayraperezdelgado@gmail.com>
2023-02-09 10:16:54 +0000

    Movimiento nave terminado

--- Juegonave/Assets/Script/MovimientoAliado.cs
+++ Juegonave/Assets/Script/MovimientoAliado.cs
@@ -3,27 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MovimientoAliado : MonoBehaviour
-{
-   
-   [SerializeField]
-      float speed = 0f;
-   [SerializeField]
-       float speedRotationVertical = 10f;
-   [SerializeField]
-       float speedRotationHorizontal = 10f;
+{ 
+    [SerializeField]
+    bool throttle => Input.GetKey(KeyCode.Space);
+    [SerializeField]
+    float pitchPower, rollPower, yawPower, enginePower;
+    [SerializeField]
+    float activeRoll, activePitch, activeYaw;
 
+    void Update()
+      {
+        if (throttle)
+        {
+            transform.position += transform.forward * enginePower * Time.deltaTime;
 
+            activePitch = Input.GetAxisRaw("Vertical") * pitchPower * Time.deltaTime;
+            activeRoll = Input.GetAxisRaw("Horizontal") * rollPower * Time.deltaTime;
+            activeYaw = Input.GetAxisRaw("Yaw") * yawPower * Time.deltaTime;
 
+            transform.Rotate(activePitch * pitchPower * Time.deltaTime,
+                activeYaw * yawPower * Time.deltaTime,
+                -activeRoll * rollPower * Time.deltaTime,
+                Space.Self);
+        }
+        else
+        {
+            activePitch = Input.GetAxisRaw("Vertical") * (pitchPower / 2) * Time.deltaTime;
+            activeRoll = Input.GetAxisRaw("Horizontal") * (rollPower / 2) * Time.deltaTime;
+            activeYaw = Input.GetAxisRaw("Yaw") * (yawPower / 2) * Time.deltaTime;
 
-   void Update()
-   {
-       float rotationx = Input.GetAxis("Vertical") * speedRotationVertical * Time.deltaTime;
-       float rotationz = Input.GetAxis("Horizontal") * speedRotationHorizontal * Time.deltaTime;
-
-       transform.Rotate(rotationz, 0f, rotationx, Space.Self);
-
-       transform.position += transform.localPosition * Time.deltaTime * speed;
-
-   }
-   
+            transform.Rotate(activePitch * pitchPower * Time.deltaTime,
+                activeYaw * yawPower * Time.deltaTime,
+                -activeRoll * rollPower * Time.deltaTime,
+                Space.Self);
+        }
+    }
 }
